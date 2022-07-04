6075ea59c54259187358a6aedd7d386e8b7af02c
Abbas4tech <shaikhabbas54@gmail.com>
2022-07-05 03:52:38 +0530

    Reduce code

--- scripts/app.js
+++ scripts/app.js
@@ -1,79 +1,10 @@
+import { buttons } from "./data.js";
+
 const root = document.getElementById("root");
 const display = document.createElement("input");
 display.readOnly = true;
 root.append(display);
 let operationIsDone = false;
-
-const buttons = [
-  {
-    value: "0",
-    onClick: () => clickHandler("0"),
-  },
-  {
-    value: "1",
-    onClick: () => clickHandler("1"),
-  },
-  {
-    value: "2",
-    onClick: () => clickHandler("2"),
-  },
-  {
-    value: "3",
-    onClick: () => clickHandler("3"),
-  },
-  {
-    value: "4",
-    onClick: () => clickHandler("4"),
-  },
-  {
-    value: "5",
-    onClick: () => clickHandler("5"),
-  },
-  {
-    value: "6",
-    onClick: () => clickHandler("6"),
-  },
-  {
-    value: "7",
-    onClick: () => clickHandler("7"),
-  },
-  {
-    value: "8",
-    onClick: () => clickHandler("8"),
-  },
-  {
-    value: "9",
-    onClick: () => clickHandler("9"),
-  },
-  {
-    value: "+",
-    onClick: () => clickHandler("+"),
-  },
-  {
-    value: "-",
-    onClick: () => clickHandler("-"),
-  },
-  {
-    value: "*",
-    onClick: () => clickHandler("*"),
-  },
-  {
-    value: "/",
-    onClick: () => clickHandler("/"),
-  },
-  {
-    value: ".",
-    onClick: () => clickHandler("."),
-  },
-  {
-    value: "=",
-    onClick: () => result(),
-  },
-  {
-    value: "Back",
-    onClick: () => back(),
-  },
-];
 
 const clickHandler = (value) => {
   if (operationIsDone) {
@@ -95,13 +26,15 @@ const result = () => {
   display.value = eval(display.value).toFixed(2);
 };
 
-const renderer = (buttonsArray) => {
-  buttonsArray.map(({ value, onClick }) => {
+buttons
+  .map((e) => ({
+    value: e,
+    onClick: () =>
+      e === "Back" ? back() : e === "=" ? result() : clickHandler(e),
+  }))
+  .forEach(({ value, onClick }) => {
     const buttonElem = document.createElement("button");
     buttonElem.textContent = value;
     root.append(buttonElem);
     buttonElem.addEventListener("click", onClick);
   });
-};
-
-renderer(buttons);
